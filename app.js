365174086032d11f08061c8ffe16c13a0f4ecf08
mlisareyes <mlisareyes@gmail.com>
2021-01-31 22:03:15 -0500

    added book constructor and class

---
+++ app.js
@@ -0,0 +1,18 @@
+// Book Class: Represents a book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// UI Class: Handle UI Tasks
+
+// Store Class: Handles Storage
+
+// Event: Display MyBookList
+
+// Event: Add a book
+
+//Event: Remove a Book
