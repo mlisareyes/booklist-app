9acf4af535d79b7d36483db389fd0e9b3ab9fc5c
mlisareyes <mlisareyes@gmail.com>
2021-02-16 01:22:15 -0500

    added event listeners

--- app.js
+++ app.js
@@ -24,13 +24,45 @@ class UI {
     ];
 
     const books = StoredBooks;
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class ="btn btn-danger btn-sm
+    delete">X</a></td>
+    `;
+
+    list.appendChild(row);
   }
  }
 
 // Store Class: Handles Storage
 
 // Event: Display MyBookList
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a book
+document.querySelector('#book-form').addEventListener('submit', (event) =>
+{
+  event.preventDefault();
+  //Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  console.log(book)
+});
 
 //Event: Remove a Book
